d3c6e400a743c8a4c21fcb7a0eef8d612a59243e
Hammad-ul-Hassan <103960660+HammadulHassan5812@users.noreply.github.com>
2025-07-20 02:03:12 +0500

    added navlinks

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,35 @@ import Home from './pages/Home'
 import Product from './pages/Product'
 import About from './pages/About'
 import Contact from './pages/Contact'
- import { Routes,Route } from 'react-router'
+ import { RouterProvider,Route, createBrowserRouter, createRoutesFromElements } from 'react-router'
+import RootLayout from './Layout/RootLayout'
  const App = () => {
+
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<RootLayout/>} >
+          <Route index element={<Home/>} />
+          <Route path="product" element={<Product/>} />
+          <Route path="about" element={<About/>} />
+          <Route path="contact" element={<Contact/>} />
+        </Route>
+      
+    )
+  )
+
    return (
-     <div>
-      <Navbar/>
-       <div className='container'>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/Product' element={<Product/>} />
-        <Route path='/About' element={<About/>} />
-        <Route path='/Contact' element={<Contact/>} />
-      </Routes>
-      </div>
-     </div>
+    //  <div>
+    //   {/* <Navbar/>
+    //    <div className='container'>
+    //   <Routes>
+    //     <Route path='/' element={<Home/>} />
+    //     <Route path='/Product' element={<Product/>} />
+    //     <Route path='/About' element={<About/>} />
+    //     <Route path='/Contact' element={<Contact/>} />
+    //   </Routes>
+    //   </div> */}
+    //  </div>
+    <RouterProvider router={router} />
    )
  }
  
