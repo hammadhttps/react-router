eab689e3a98c441906158795ebbd0e1e177af408
Hammad-ul-Hassan <103960660+HammadulHassan5812@users.noreply.github.com>
2025-07-20 01:19:11 +0500

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
  import React from 'react'
- 
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Product from './pages/Product'
+import About from './pages/About'
+import Contact from './pages/Contact'
+ import { Routes,Route } from 'react-router'
  const App = () => {
    return (
-     <div>App</div>
+     <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Product' element={<Product/>} />
+        <Route path='/About' element={<About/>} />
+        <Route path='/Contact' element={<Contact/>} />
+      </Routes>
+     </div>
    )
  }
  
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src="hh" alt="logo Image" />
+        <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        <button>Get Started</button>
+    </div>
+  )
+}
+
+export default Navbar
